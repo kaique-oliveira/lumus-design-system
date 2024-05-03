79e62890f62ae63e0ed87e8cdb2b95bcc1a71d2a
kah230895 <kaiqueoliveiraantonio@gmail.com>
2024-05-03 18:19:54 -0300

    Teste

--- packages/react/src/components/DataTable/styles.ts
+++ packages/react/src/components/DataTable/styles.ts
@@ -3,7 +3,7 @@ import { keyframes, styled } from "../../styles";
 
 export const Table = styled("div", {
   width: "max-content",
-  background: "$main200",
+  background: "$main300",
   padding: "$4",
   borderRadius: "$sm",
   boxShadow: "rgba(0, 0, 0, 0.15) 0px 5px 15px 0px",
